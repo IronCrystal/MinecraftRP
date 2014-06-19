d6611c310a9f96576f78508289ca262b7d922e98
IronCrystal <apeace95@gmail.com>
2014-06-19 16:09:30 -0400

    Fixed World Guard Dependency

--- src/ironcrystal/minecraftrp/MinecraftRP.java
+++ src/ironcrystal/minecraftrp/MinecraftRP.java
@@ -3,21 +3,19 @@ package ironcrystal.minecraftrp;
 import ironcrystal.minecraftrp.commands.OccupationalCommands;
 import ironcrystal.minecraftrp.event.Listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class MinecraftRP extends JavaPlugin {
-		
+
 	@Override
 	public void onEnable() {
 		Files.initializeFiles();
 		Permissions.initializePermissions();
 		Listeners.registerEvents(this);
-		
+
 		/**
 		 * Commands
 		 */
@@ -25,23 +23,23 @@ public class MinecraftRP extends JavaPlugin {
 		getCommand("score").setExecutor(commands);
 		getCommand("rp").setExecutor(commands);
 	}
-	
+
 	@Override
 	public void onDisable() {
-		
+
 	}
-	
+
 	public MinecraftRP getPlugin() {
 		return this;
 	}
-	
+
 	public WorldGuardPlugin getWorldGuard() {
-	    Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
-	 
-	    if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
-	        return null; // Maybe you want throw an exception instead
-	    }
-	 
-	    return (WorldGuardPlugin) plugin;
+		Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+
+		if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+			return null; // Maybe you want throw an exception instead
+		}
+
+		return (WorldGuardPlugin) plugin;
 	}
 }
