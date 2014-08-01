8fc58de927c47fd3194932918a7fe3e519f9ee01
IronCrystal <apeace95@gmail.com>
2014-08-01 09:13:30 -0400

    Added Change Occupation Wall Signs

--- src/ironcrystal/minecraftrp/event/ChangeOccupation.java
+++ src/ironcrystal/minecraftrp/event/ChangeOccupation.java
@@ -23,7 +23,7 @@ public class ChangeOccupation implements Listener {
 		OccupationalPlayer p = new OccupationalPlayer(player.getUniqueId());
 		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			Block block = event.getClickedBlock();
-			if (block.getType() == Material.SIGN || block.getType() == Material.SIGN_POST) {
+			if (block.getType() == Material.SIGN || block.getType() == Material.SIGN_POST || block.getType() == Material.WALL_SIGN) {
 				Sign sign = (Sign) block.getState();
 				if (sign.getLine(0).equalsIgnoreCase(ChatColor.BOLD + "[MinecraftRP]") 
 						&& sign.getLine(2).equalsIgnoreCase(ChatColor.BOLD + "Change Occup")) {
